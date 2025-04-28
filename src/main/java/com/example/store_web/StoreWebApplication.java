5b9e03f51c744f62770a9c13deb236ab8e0ad141
Mey-29 <madeleyvalencia13@gmail.com>
2025-04-27 23:42:43 -0500

    feat: add configuration database connection

--- src/main/java/com/example/store_web/StoreWebApplication.java
+++ src/main/java/com/example/store_web/StoreWebApplication.java
@@ -7,6 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreWebApplication {
   public static void main(String[] args) {
     SpringApplication.run(StoreWebApplication.class, args);
-    System.out.println("Hola Mundo");
   }
 }
